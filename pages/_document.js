1de74637613f8233c89b4bdbd76717c8798cf3f1
RavelloH <ravellohh@gmail.com>
2026-03-08 16:04:07 +0800

    chore: 修改统计地址

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-          {/* <script defer src="https://analytics.ravelloh.top/script.js" data-website-id="8ef18ba2-4313-457b-b6f1-a36ef1e1a27c"></script> */}
+          <script defer src="https://insight.ravelloh.com/script.js?siteId=097654ef-9484-4e52-b5fe-b3d3307da1e2"></script>
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="theme-color" content="#ffffff" />
           <script src="/register-sw.js" defer></script>
